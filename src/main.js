22020f6395c74fe44384a4b05e080dd5a2d34972
Rachel Schieferstein <violenza@gmail.com>
2020-02-17 13:12:05 -0800

    html base, trying to get map to work

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import { GeoService } from "./../src/geo-service.js";
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+
+$(document).ready(function() {
+  $("#addressForm").submit(function() {
+    event.preventDefault();
+    // const address = $("#address").val();
+ 
+
+
+  });
+});
